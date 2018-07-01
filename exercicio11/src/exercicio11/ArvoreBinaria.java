61eff7ed0426649a4bea604c5eacd1d40d874091
leoro <leoro@DESKTOP-LJM33BM>
2018-07-01 20:42:18 -0300

    Exercicio 11

---
+++ exercicio11/src/exercicio11/ArvoreBinaria.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio11;
+
+/**
+ *
+ * @author leoro
+ * @param <T>
+ */
+public class ArvoreBinaria<T extends Comparable<T>> extends ArvoreBinariaAbstract<T> {
+
+    @Override
+    public void setRaiz(NoArvoreBinaria<T> raiz) {
+        super.setRaiz(raiz); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public NoArvoreBinaria<T> buscar(T info) {
+        NoArvoreBinaria p = getRaiz();
+        while (p != null && (info.compareTo((T) p.getInfo()) != 0)) {
+            if (info.compareTo((T) p.getInfo()) < 0) {
+                p = p.getEsquerda();
+            } else {
+                p = p.getDireita();
+            }
+        }
+        return p;
+    }
+
+    public void Inserir(T info) {
+        NoArvoreBinaria novo = new NoArvoreBinaria(info);
+        NoArvoreBinaria p = getRaiz();
+        NoArvoreBinaria pai = null;
+        if (p == null) {
+            setRaiz(novo);
+        } else {
+            p = getRaiz();
+            while (true) {
+                pai = p;
+                if (info.compareTo((T) p.getInfo()) < 0) {
+                    p = pai.getEsquerda();
+                    if (p == null) {
+                        pai.setEsquerda(novo);
+                        return;
+                    }
+                } else {
+                    p = pai.getDireita();
+                    if (p == null) {
+                        pai.setDireita(novo);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void retirar(T info) {
+        NoArvoreBinaria p = getRaiz();
+        NoArvoreBinaria pai = null;
+        boolean filhoEsquerda = false;
+
+        while (p != null && (info.compareTo((T) p.getInfo()) != 0)) {
+            pai = p;
+            if (info.compareTo((T) p.getInfo()) < 0) {
+                filhoEsquerda = true;
+                p = p.getEsquerda();
+            } else {
+                filhoEsquerda = false;
+                p = p.getDireita();
+            }
+        }
+        if (p != null) {// Folha
+            if (p.getEsquerda() == null && p.getDireita() == null) {
+                if (p == getRaiz()) {
+                    setRaiz(null);
+                } else {
+                    if (filhoEsquerda == true) {
+                        pai.setEsquerda(null);
+                    } else {
+                        pai.setDireita(null);
+                    }
+                }
+            } else {
+                if (p.getDireita() == null) {//No Com um filho
+                    if (p == getRaiz()) {
+                        setRaiz(p.getEsquerda());
+                    } else {
+                        if (filhoEsquerda == true) {
+                            pai.setEsquerda(p.getEsquerda());
+                        } else {
+                            pai.setDireita(p.getEsquerda());
+                        }
+                    }
+                } else {
+                    if (p.getEsquerda() == null) {
+                        if (p == getRaiz()) {
+                            setRaiz(p.getEsquerda());
+                        } else {
+                            if (filhoEsquerda == true) {
+                                pai.setEsquerda(p.getDireita());
+                            } else {
+                                pai.setDireita(p.getDireita());
+                            }
+                        }
+                    } else {//nó com 2 filhos
+                        NoArvoreBinaria sucessor = extrairSucessor(p);
+                        if (p == getRaiz()) {
+                            setRaiz(sucessor);
+                        } else {
+                            if (filhoEsquerda == true) {
+                                pai.setEsquerda(sucessor);
+                            } else {
+                                pai.setDireita(sucessor);
+                            }
+                        }
+                        sucessor.setEsquerda(p.getEsquerda());
+                    }
+                }
+
+            }
+        }
+    }
+
+    public NoArvoreBinaria extrairSucessor(NoArvoreBinaria excluir) {
+        NoArvoreBinaria p = excluir.getDireita();
+        NoArvoreBinaria paiSucessor = excluir;
+        NoArvoreBinaria sucessor = excluir;
+
+        while (p != null) {
+            paiSucessor = sucessor;
+            sucessor = p;
+            p = p.getEsquerda();
+        }
+        if (sucessor != excluir.getDireita()) {
+            paiSucessor.setEsquerda(sucessor.getDireita());
+            sucessor.setDireita(excluir.getDireita());
+        }
+        return sucessor;
+
+    }
+}
---
+++ exercicio11/src/exercicio11/ArvoreBinariaAbstract.java
@@ -0,0 +1,67 @@
+package exercicio11;
+
+public abstract class ArvoreBinariaAbstract<T> {
+
+    private NoArvoreBinaria<T> raiz;
+
+    public ArvoreBinariaAbstract() {
+        raiz = null;
+    }
+
+    public void setRaiz(NoArvoreBinaria<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public NoArvoreBinaria<T> getRaiz() {
+        return raiz;
+    }
+    
+
+    public boolean estaVazia() {
+        return raiz == null;
+    }
+    public boolean pertence(T info) {
+        return (buscar(raiz, info) != null);
+    }
+
+    public abstract NoArvoreBinaria<T> buscar(T info); //{
+    //    return (buscar(raiz, info));
+  //  }
+
+    private NoArvoreBinaria<T> buscar(NoArvoreBinaria<T> no, T info) {
+        if (no == null || no.getInfo().equals(info))
+            return no;
+        NoArvoreBinaria<T> t = buscar(no.getEsquerda(), info);
+        if (t != null)
+            return t;
+        return buscar(no.getDireita(), info);
+    }
+
+    @Override
+    public String toString() {
+        return arvorePre(raiz);
+    }
+
+    private String arvorePre(NoArvoreBinaria<T> no) {
+        if (no == null)
+            return "<>";
+        return "<" + no.getInfo() + (arvorePre(no.getEsquerda()) + arvorePre(no.getDireita())) + ">";
+    }
+
+
+    public int contarNos() {
+        return contarNos(raiz, 0);
+    }
+
+    private int contarNos(NoArvoreBinaria<T> no, int quantidade) {
+        if (no == null)
+            return quantidade;
+        quantidade++;
+        if (no.getEsquerda() != null)
+            return quantidade + contarNos(no.getEsquerda(), quantidade);
+        if (no.getDireita() != null)
+            return quantidade + contarNos(no.getDireita(), quantidade);
+        return quantidade;
+    }
+
+}
