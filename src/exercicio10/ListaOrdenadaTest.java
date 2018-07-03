35b3fb5be672515a12d8afac5e6f4296367072b5
Patrick <patrick.krauss@outlook.com>
2018-07-03 16:34:15 -0300

    ajustes

--- src/exercicio10/ListaOrdenadaTest.java
+++ src/exercicio10/ListaOrdenadaTest.java
@@ -19,54 +19,31 @@ public class ListaOrdenadaTest {
     @Test
     public void testInserir() {
 
-        System.out.println("inserir");
-
-        ListaOrdenada instance = new ListaOrdenada();
+        ListaOrdenada<Integer> instance = new ListaOrdenada<Integer>();
 
         instance.inserir(100);
-
         instance.inserir(20);
-
         instance.inserir(70);
-
         instance.inserir(1);
 
-
-
         assertEquals("1,20,70,100", instance.toString());
-
     }
 
     @Test
     public void testBuscar1() {
-
-        System.out.println("buscar1");
-
-        ListaOrdenada instance = new ListaOrdenada();
+        ListaOrdenada<Integer> instance = new ListaOrdenada<Integer>();
 
         instance.inserir(0);
-
         instance.inserir(10);
-
         instance.inserir(20);
-
         instance.inserir(30);
-
         instance.inserir(40);
-
         instance.inserir(50);
-
         instance.inserir(60);
-
         instance.inserir(70);
-
         instance.inserir(80);
-
         instance.inserir(90);
-
         instance.inserir(100);
-
-
 
         assertEquals(2, instance.buscar(20));
 
@@ -75,33 +52,18 @@ public class ListaOrdenadaTest {
     @Test
     public void testBuscar2() {
 
-        System.out.println("buscar2");
-
-        ListaOrdenada instance = new ListaOrdenada();
-
+        ListaOrdenada<Integer> instance = new ListaOrdenada<Integer>();
         instance.inserir(0);
-
         instance.inserir(10);
-
         instance.inserir(20);
-
         instance.inserir(30);
-
         instance.inserir(40);
-
         instance.inserir(50);
-
         instance.inserir(60);
-
         instance.inserir(70);
-
         instance.inserir(80);
-
         instance.inserir(90);
-
         instance.inserir(100);
-
-
 
         assertEquals(4, instance.buscar(40));
 
@@ -110,104 +72,60 @@ public class ListaOrdenadaTest {
     @Test
     public void testBuscar3() {
 
-        System.out.println("buscar3");
-
-        ListaOrdenada instance = new ListaOrdenada();
+        ListaOrdenada<Integer> instance = new ListaOrdenada<Integer>();
 
         instance.inserir(0);
-
         instance.inserir(10);
-
         instance.inserir(20);
-
         instance.inserir(30);
-
         instance.inserir(40);
-
         instance.inserir(50);
-
         instance.inserir(60);
-
         instance.inserir(70);
-
         instance.inserir(80);
-
         instance.inserir(90);
-
         instance.inserir(100);
 
-
-
         assertEquals(7, instance.buscar(70));
-
     }
 
     @Test
     public void testBuscar4() {
 
-        System.out.println("buscar4");
-
-        ListaOrdenada instance = new ListaOrdenada();
+        ListaOrdenada<Integer> instance = new ListaOrdenada<Integer>();
 
         instance.inserir(0);
-
         instance.inserir(10);
-
         instance.inserir(20);
-
         instance.inserir(30);
-
         instance.inserir(40);
-
         instance.inserir(50);
-
         instance.inserir(60);
-
         instance.inserir(70);
-
         instance.inserir(80);
-
         instance.inserir(90);
-
         instance.inserir(100);
 
-
-
         assertEquals(10, instance.buscar(100));
-
     }
 
     @Test
     public void testBuscar5() {
 
-        System.out.println("buscar5");
-
-        ListaOrdenada instance = new ListaOrdenada();
+        ListaOrdenada<Integer> instance = new ListaOrdenada<Integer>();
 
         instance.inserir(0);
-
         instance.inserir(10);
-
         instance.inserir(20);
-
         instance.inserir(30);
-
         instance.inserir(40);
-
         instance.inserir(50);
-
         instance.inserir(60);
-
         instance.inserir(70);
-
         instance.inserir(80);
-
         instance.inserir(90);
-
         instance.inserir(100);
 
         assertEquals(-1, instance.buscar(85));
-
     }
-
 }
